74e2eb306102731137761f9288ea4b1bca813ecf
Mark Belyaev <163128703+trxher@users.noreply.github.com>
2024-03-16 01:49:29 +0500

    changes

--- main.go
+++ main.go
@@ -5,5 +5,31 @@ import (
 )
 
 func main() {
-	fmt.Print("Hello Go")
+	fmt.Println("Введи операцию: '+' '-' '*' '/'")
+	var operation string
+	fmt.Scan(&operation)
+
+	fmt.Println("Введи первую переменную:")
+	var a float64
+	fmt.Scan(&a)
+
+	fmt.Println("Введи вторую переменную:")
+	var b float64
+	fmt.Scan(&b)
+
+	if operation == "+" {
+		fmt.Print("Ответ: ", a+b)
+	} else if operation == "-" {
+		fmt.Print("Ответ: ", a-b)
+	} else if operation == "*" {
+		fmt.Print("Ответ: ", a*b)
+	} else if operation == "/" {
+		if b == 0 {
+			fmt.Print("На ноль делить нельзя!")
+		} else {
+			fmt.Print("Ответ: ", a/b)
+		}
+	} else {
+		fmt.Print("Такой операции не существует!")
+	}
 }
