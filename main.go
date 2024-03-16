95a30d767b209b722654e8e2ba48231b60f34511
Mark Belyaev <163128703+trxher@users.noreply.github.com>
2024-03-16 18:26:13 +0500

    changes

--- main.go
+++ main.go
@@ -6,43 +6,64 @@ import (
 	"os"
 )
 
-func main() {
-	fmt.Println("Введи операцию: '+' '-' '*' '/'")
-	var operation string
-	fmt.Scan(&operation)
+var a, b float64
+var operation string
 
+// Ввод знака операции
+func inputOperationUser() {
+	fmt.Println("Введи операцию:\n'+' - сложение\n'-' - вычитание\n'*' - умножение\n'/' - деление")
+	fmt.Scan(&operation)
+}
+
+// Ввод переменных
+func inputVariablesUser() {
 	fmt.Println("Введи первую переменную:")
-	var a float64
 	fmt.Scan(&a)
 
 	fmt.Println("Введи вторую переменную:")
-	var b float64
 	fmt.Scan(&b)
+}
 
+// Математические вычесления
+func calc() {
 	if operation == "+" {
-		fmt.Println("Ответ: ", a+b)
+		fmt.Println("Ответ:", a+b)
 	} else if operation == "-" {
-		fmt.Println("Ответ: ", a-b)
+		fmt.Println("Ответ:", a-b)
 	} else if operation == "*" {
-		fmt.Println("Ответ: ", a*b)
+		fmt.Println("Ответ:", a*b)
 	} else if operation == "/" {
 		if b == 0 {
 			fmt.Println("На ноль делить нельзя!")
 		} else {
-			fmt.Println("Ответ: ", a/b)
+			fmt.Println("Ответ:", a/b)
 		}
 	} else {
-		fmt.Println("Такой операции не существует!")
+		fmt.Print("Неверная операция!")
 	}
+}
 
+// Обработчик выхода из программы
+func exit() {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for scanner.Scan() {
 		exit := scanner.Text()
+
 		if exit == "q" {
 			break
 		} else {
 			fmt.Println("Нажмите 'q' для выхода")
 		}
 	}
+}
+
+// Основная функция
+func main() {
+
+	inputOperationUser()
+	inputVariablesUser()
+	calc()
+
+	exit()
 }
